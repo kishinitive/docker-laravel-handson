b71d180b6e68ffc7ad828b38b33d3ee40eeba463
幸村紀信 <gips.komura@gmail.com>
2022-06-12 13:57:51 +0900

    トップページを作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use app\Http\Controllers\MemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\MemoController@index');
+// Route::get('/', function () {
+//     return view('memos.index');
+// });
